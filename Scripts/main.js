e2cdb779c1d1533b4eb6071b5e18ea4d4eab3dd9
joshpters <joshuapetersmail@gmail.com>
2020-10-01 12:13:10 -0400

    Updated layout & design
    Many layout & design changes

--- Scripts/main.js
+++ Scripts/main.js
@@ -149,12 +149,16 @@
   // Porfolio isotope and filter
   $(window).on('load', function() {
     var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
+        itemSelector: '.portfolio-item'
+    });
+
+    portfolioIsotope.isotope({
+        filter: $("#portfolio-flters li").data('filter')
     });
 
     $('#portfolio-flters li').on('click', function() {
       $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
+        $(this).addClass('filter-active');
 
       portfolioIsotope.isotope({
         filter: $(this).data('filter')
